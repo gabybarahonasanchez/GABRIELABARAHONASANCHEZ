bbfa4a16a3e25a734e477a55d5e5f0eaaa21229b
gabybarahonasanchez <gabybarahonasanchez@gmail.com>
2026-06-02 13:37:17 -0600

    Create worldcup.sql

---
+++ worldcup.sql
@@ -0,0 +1,14 @@
+CREATE TABLE teams(
+  team_id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE games(
+  game_id SERIAL PRIMARY KEY,
+  year INT NOT NULL,
+  round VARCHAR(50) NOT NULL,
+  winner_id INT NOT NULL REFERENCES teams(team_id),
+  opponent_id INT NOT NULL REFERENCES teams(team_id),
+  winner_goals INT NOT NULL,
+  opponent_goals INT NOT NULL
+);
